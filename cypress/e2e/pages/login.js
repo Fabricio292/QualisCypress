3cea76847289653482af717968b34f5d8acaf45d
Fabricio292 <114675690+Fabricio292@users.noreply.github.com>
2022-11-01 11:54:58 -0300

    Finalización de los "When"

--- cypress/e2e/pages/login.js
+++ cypress/e2e/pages/login.js
@@ -6,6 +6,7 @@ class loginPage {
       passwordInput: () => cy.get("#password"),
       loginBtn: () => cy.get("#login-button"),
       errorMessage: () => cy.get('h3[data-test="error"]'),
+      orderBtn: () => cy.get('[data-test="product_sort_container"]')
     };
   }
   module.exports = new loginPage();
--- cypress/e2e/step_definitions/demo.js
+++ cypress/e2e/step_definitions/demo.js
@@ -1,4 +1,4 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, When, Then} from "@badeball/cypress-cucumber-preprocessor";
 const loginPage = require('../pages/login')
 
 Given("A user opens a saucelabs website", function () {
@@ -10,3 +10,22 @@ When("A user enters the username {string}", function (username) {
 
 });
 
+When("A user enters the password {string}", function (password) {
+    loginPage.elements.passwordInput().type(password)
+
+});
+
+When("A user clicks on the login button", function () {
+    loginPage.elements.loginBtn().click()
+
+});
+
+When("A user order by {string}", function (order) {
+    loginPage.elements.orderBtn().select(order)
+
+});
+
+Then("A user order by {string}", function (order) {
+    loginPage.elements.orderBtn().select(order)
+
+});
